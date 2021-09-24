90172842d3d571bc8ac586e58625296630a7fc88
mhanifm <mhanifmuttaqin95@gmail.com>
2021-09-24 08:36:32 +0700

    revisi api doc

--- controllers/countryNameController.js
+++ controllers/countryNameController.js
@@ -6,6 +6,9 @@ class CountryNameController {
         .then(({ data }) => {
             res.status(200).json(data.response)
         })
+        .catch(err => {
+            res.status(500).json({ message: 'Internal server error' })
+        })
     }
 }
 
